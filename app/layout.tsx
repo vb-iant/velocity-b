df25944963e0d6c3e35bbcc307af834c616224ac
vb-iant <ian@velocity-b.com>
2026-08-03 15:39:33 +0100

    Add Google Tag Manager (GTM-MCKMKJZ4)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Space_Grotesk, Work_Sans } from "next/font/google";
+import { GoogleTagManager } from "@next/third-parties/google";
 import "./globals.css";
 import { SiteHeader } from "@/components/SiteHeader";
 import { SiteFooter } from "@/components/SiteFooter";
@@ -25,7 +26,16 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${spaceGrotesk.variable} ${workSans.variable}`}>
+      <GoogleTagManager gtmId="GTM-MCKMKJZ4" />
       <body className="font-body bg-white text-navy">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MCKMKJZ4"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <SiteHeader />
         {children}
         <SiteFooter />
